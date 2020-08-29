ae8a2bea925e589bce4dae430cc8155307738e4d
Melanie Thewlis <melaniethewlis@littlewebgiants.com>
2020-08-29 20:36:37 +1000

    initial commit

---
+++ public/precache-manifest.7722a30fdf370ced29c66f4c11b14ab7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac17cb9683551530b23",
+    "url": "/css/app.9c164754.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8df99fdef9f1856491eda1459a3ff9b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8ebab2e774d88a94e27",
+    "url": "/js/about.476ae516.js"
+  },
+  {
+    "revision": "3ac17cb9683551530b23",
+    "url": "/js/app.5bfa178c.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "27b1ab3fec5c4c6e5b08437fcc4becdc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
